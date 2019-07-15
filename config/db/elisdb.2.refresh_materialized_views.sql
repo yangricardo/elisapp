86db999fc2992f38574ac1e4f9d3d05ddb377f8f
Yang  Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2019-07-15 11:03:17 -0300

    ajustes visões

---
+++ config/db/elisdb.2.refresh_materialized_views.sql
@@ -0,0 +1,8 @@
+select refreshallmaterializedviews('public', false);
+
+-- REFRESH MATERIALIZED VIEW public.api_mview_processo WITH DATA;
+-- REFRESH MATERIALIZED VIEW public.api_mview_advogado_processo WITH DATA;
+-- REFRESH MATERIALIZED VIEW public.api_mview_documento_processo WITH DATA;
+-- REFRESH MATERIALIZED VIEW public.api_mview_personagens_processo WITH DATA;
+-- REFRESH MATERIALIZED VIEW public.api_mview_sentencas WITH DATA;
+-- REFRESH MATERIALIZED VIEW public.api_mview_processossimilares WITH DATA;
